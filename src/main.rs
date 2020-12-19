ac524bbf4c0253e461396df67b33ad975e513ef5
Arkadiusz /kitarek/ Kita <kitarek@gmail.com>
2020-12-19 21:59:28 +0100

    Support for multiple lines and trim spaces

--- src/main.rs
+++ src/main.rs
@@ -20,43 +20,55 @@ struct Cli {
 
 #[derive(Clone)]
 struct FileCell {
-    name: String,
-    line: String
+    name:        String,
+    extra_lines: Vec<String>
 }
 
 struct FileCellLength {
-    name_len: usize,
-    line_len: usize
+    name_len:        usize,
+    cols_num:        usize,
+    extra_line_lens: Vec<usize>
 }
 
-fn load_file(path :String) -> std::io::Result<String> {
+fn load_file(path :String) -> std::io::Result<Vec<String>> {
     let f = File::open(&path)?;
     let reader = BufReader::new(f);
-    let mut line = String::new();
+    let mut counter = 0;
+    let mut lines = Vec::new();
     for new_line in reader.lines() {
-        line = new_line.unwrap_or("".to_string());
-        break;
+        counter += 1;
+        let line = new_line.unwrap_or("".to_string());
+        if counter > 10 {
+            break;
+        }
+        lines.push(line);
     }
-    Ok(line)
+    Ok(lines)
 }
 
 fn main() {
     let args = Cli::from_args();
     if let Ok(entries) = fs::read_dir(&args.path) {
         let mut lines: Vec<FileCell> = Vec::new();
-        let mut len: FileCellLength = FileCellLength {name_len: 0, line_len: 0};
+        let mut len: FileCellLength = FileCellLength {name_len: 0, cols_num: 0, extra_line_lens: std::iter::repeat(0).take(10).collect::<Vec<_>>()};
         for entry in entries {
             if let Ok(entry) = entry {
                 if let Ok(file_type) = entry.file_type() {
                     if file_type.is_file() {
-                        if let Ok(line) = load_file(entry.path().into_os_string().into_string().unwrap()) {
+                        if let Ok(top_10_lines) = load_file(entry.path().into_os_string().into_string().unwrap()) {
                             let cell = FileCell {
                                 name: entry.file_name().to_str().unwrap_or("-/-").to_string(),
-                                line: line
+                                extra_lines: top_10_lines
                             };
                             lines.push(cell.clone());
                             len.name_len = cmp::max(len.name_len, cell.name.chars().count());
-                            len.line_len = cmp::max(len.line_len, cell.line.chars().count());
+                            let mut index = 0;
+                            for extra_line in &cell.extra_lines {
+                                let current_len = len.extra_line_lens[index];
+                                len.extra_line_lens[index] = cmp::max(current_len, extra_line.chars().count());
+                                index += 1;
+                            }
+                            len.cols_num = cmp::max(len.cols_num, index);
                         } else {
                             eprintln!("Couldn't read file {:?}", &entry.path());
                         }
@@ -68,7 +80,17 @@ fn main() {
             }
         }
         for line in lines {
-            println!("{:width1$}  {:width2$}", &line.name, &line.line, width1 = &len.name_len, width2 = &len.line_len);
+          print!("{:width1$}", &line.name, width1 = &len.name_len);
+          let mut index = 0;
+          let cols_num = line.extra_lines.len();
+          for extra_line in &line.extra_lines {
+            let effective_width = if index < cols_num - 1 { len.extra_line_lens[index] } else { 0usize };
+            if extra_line.chars().count() > 0usize || effective_width > 0usize {
+              print!("  {:width1$}", if effective_width == 0usize { extra_line.trim_end() } else { extra_line }, width1 = effective_width);
+            }
+            index += 1;
+          }
+          println!("");
         }
     } else {
         eprintln!("The path: {:?} is not a directory", &args.path);
